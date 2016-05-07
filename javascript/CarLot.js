b131cb3d21823750974b254a6e0ccfbb921ef1ae
Sulaiman.Allen <xalerons@gmail.com>
2016-05-07 13:14:56 -0500

    Switching from external functions to iife

--- javascript/CarLot.js
+++ javascript/CarLot.js
@@ -6,18 +6,62 @@ var CarLot = (function(carObj) {
   return {
     getInventory: function () {
       return inventory;
+    }, 
+    setInventory: function () { // Experimental
+      var carList = JSON.parse(event.currentTarget.responseText);
+      var carArray = [];
+      for (i = 0; i < carList.cars.length; i++) {
+        carArray[carArray.length] = (carList.cars[i]);
+      };
+      inventory = carArray;
+      CarLot.populatePage(inventory);
     },
     loadInventory: function () {
       // Create XHR object
       var inventoryLoader = new XMLHttpRequest();
       // Then tell the XHR object exactly what to do
-      // inventoryLoader.addEventListener("load", populatePage(event));
       inventoryLoader.addEventListener("load", function(event) {
           populatePage(event);
         });
       inventoryLoader.open("GET", "json/inventory.json");
       // Tell the XHR object to start
       inventoryLoader.send();
+    }
+    populatePage: function (event) {
+
+      console.log("In populatePage");
+      var carList = JSON.parse(event.currentTarget.responseText);
+
+      // Loop over the inventory and populate the page
+      domString = `<div class="row">`;
+
+      for(var i = 0; i < carList.cars.length; i++) {
+        // console.log("car", carList.cars[i]);
+        make = carList.cars[i].make;
+        model = carList.cars[i].model;
+        year = carList.cars[i].year;
+        price = carList.cars[i].price;
+        color = carList.cars[i].color;
+
+        domString += `<div class="col-sm-4" id="div${color}"><h3>${make} ${model}</h3>`
+
+        if (model === "Rogue") {
+          domString += `<img src="img/rogue.jpg">`
+        } else if (model === "X5") {
+          domString += `<img src="img/x5.jpg">`
+        } else if (model === "Cayenne") {
+          domString += `<img src="img/cayenne.jpg">`
+        }
+      
+        domString += `<p>${year}</p><p>${price}</p><p>${color}</p></div>`
+      }
+
+      domString += `</div>`;
+
+      document.getElementsByClassName("container")[0].innerHTML = domString;
+
+      // Now that the DOM is loaded, establish all the event listeners needed
+      CarLot.activateEvents();
     }
   };
   
