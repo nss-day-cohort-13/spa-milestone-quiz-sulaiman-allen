32d027789ca995c404db17d3dbcd49cf00922466
xalerons <xalerons@gmail.com>
2016-05-09 01:05:24 -0500

    Fixed some bugs

--- javascript/CarEvents.js
+++ javascript/CarEvents.js
@@ -8,7 +8,32 @@ var carLot = (function(carObj) {
         // console.log("click event = ", event.target);
         // carLot.borderReset();
         // carLot.activateEvents();
-        carLot.setBorder(event.target);
+        // color = document.getElementsByClassName("container")[0].lastElementChild;//childNodes;
+        // color = event.target.lastElementChild.innerHTML;
+
+        // console.log("type of color = ", typeof color);
+        color = event.target;
+        console.log("color", color);
+
+        // color = color.innerHTML.slice(0,19)
+        // console.log("color.innerHTML.slice(0,19)", color);
+
+        // if (color.innerHTML.slice(0,19) !== '<div class="col-sm-4"') {
+        if (color.innerHTML) {
+
+          // if the color p element is selected, the color has been found
+          if(color.innerHTML.slice(0,5) === "Color") {
+            color = color.innerHTML.slice(7);
+            console.log("color = ", color);
+          }
+        // }else if (color.)
+        }
+        console.log("type of color = ", typeof color);
+        // color = event.target.lastElementChild.innerHTML.slice(0,13);
+        // <p id="color">Color: Grey</p>
+        // if (color === '<p id="color"')
+        // console.log("color", color);
+        // carLot.setBorder(event.target);
       }); //carObj.setBoarder(carElement, carColor)
     };
 
